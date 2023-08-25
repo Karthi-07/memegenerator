44f568afa81f49b67e20781e4827e272d87472ea
Karthikeyan C <karthikeyan22032003@gmail.com>
2023-08-25 20:44:05 +0530

    meme-js changes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import logo from "D:/MERN - stack/memegenerator/src/assets/unnamed.png";
 import "D:/MERN - stack/memegenerator/src/style/headerstyle.css";
 import memesData from "./memesData";
+
 const Meme = () => {
-  const [memeImage,setMemeImage] = useState("");
+  const [meme,setMeme] = useState({
+                         toptext:"top text",
+                         bottomtext:"bottom text",
+                         url:"https://i.imgflip.com/30b1gx.jpg"})
+  
   function handleclick() {
     var arr = memesData.data.memes;
     var p = Math.floor(Math.random()*arr.length);
-    setMemeImage(()=>memesData.data.memes[p].url);
+    let first = document.getElementById("up").value;
+    let last = document.getElementById("down").value;
+    console.log(first+" "+last);
+    setMeme((prev)=>{
+      return {
+        toptext:first,
+        bottomtext:last,
+        url:memesData.data.memes[p].url
+      }})
   }
   return (
     <main>
       <div className="form">
         <div className="input">
-          <input type="text" className="up" placeholder="Top text" />
-          <input type="text" className="down" placeholder="Bottom text" />
+          <input type="text" className="up" id="up" placeholder="Top text" />
+          <input type="text" className="down" id="down" placeholder="Bottom text" />
         </div>
         <br></br>
         <button className="button" onClick={handleclick}>
@@ -22,7 +35,7 @@ const Meme = () => {
         </button>
       </div>
       <div></div>
-      <img src={memeImage} className="memeimage" ></img>
+      <img src={meme.url} className="memeimage" ></img>
     </main>
   );
 };
